90692ba2ce98ac0a5feec31c1b8078811e684d1d
azamkhan50 <azamdeve939@gmail.com>
2026-06-01 18:50:31 +0500

    fix: resolve casing import mismatches for vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,10 @@
-
-// import Footer from "./components/footer";
-// import HeroSlider from "./components/hero_slider";
-// import Navbar from "./components/Navbar";
-// import Projects from "./components/project";
-// import Services from "./components/services";
-// import Testimonials from "./components/testimonal";
-// import About from "./components/about";
-
-
-// function App() {
-//   return (
-//     <>
-//     <Navbar />
-
-//       <div id="home">
-//         <HeroSlider />
-//       </div>
-//       <div id="about">
-//         <About />
-//       </div>
-
-//       <div id="services">
-//         <Services />
-//       </div>
-
-//       <div id="projects">
-//         <Projects />
-//       </div>
-
-//       <div id="about">
-//         <Testimonials />
-//       </div>
-
-//       <div id="contact">
-
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import HeroSlider from "./components/hero_slider";
 import Services from "./components/services";
 import Projects from "./components/project";
-import Testimonials from "./components/testimonal";
+import Testimonials from "./components/testimonal"; // Double check if your file is spelled "testimonal" or "testimonial"
 import About from "./components/about";
 import Footer from "./components/footer";
 import Contact from "./components/Contact";
@@ -62,10 +17,8 @@ function Home() {
       <HeroSlider />
       <Services />
       <Projects />
-        <ClientsSlider />
+      <ClientsSlider />
       <Testimonials />
-    
-     
     </>
   );
 }
@@ -80,11 +33,11 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/footer" element={<Footer />} /> */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      
       <Footer />
-        <WhatsAppButton />
+      <WhatsAppButton />
     </>
   );
 }
