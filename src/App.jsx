6e9955d5361518fa73e6f58e74a704daa3a35690
azamkhan50 <azamdeve939@gmail.com>
2026-06-01 19:20:34 +0500

    fix: match App.jsx imports exactly to sidebar filenames

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import HeroSlider from "./components/hero_slider";
-import Services from "./components/services";
-import Projects from "./components/project";
-import Testimonials from "./components/testimonal"; // Double check if your file is spelled "testimonal" or "testimonial"
-import About from "./components/about";
-import Footer from "./components/footer";
-import Contact from "./components/Contact";
-import ClientsSlider from "./components/ClientsSlider";
-import WhatsAppButton from "./components/WhatsAppButton";
+import Navbar from "./components/Navbar.jsx";
+import HeroSlider from "./components/Hero_slider.jsx";
+import Services from "./components/Services.jsx";
+import Project from "./components/Project.jsx"; // Fixed to match sidebar: Project.jsx
+import Testimonials from "./components/Testimonal.jsx"; // Matches sidebar: Testimonal.jsx
+import About from "./components/About.jsx";
+import Footer from "./components/Footer.jsx";
+import Contact from "./components/Contact.jsx";
+import ClientsSlider from "./components/ClientsSlider.jsx";
+import WhatsAppButton from "./components/WhatsAppButton.jsx";
 
 function Home() {
   return (
     <>
       <HeroSlider />
       <Services />
-      <Projects />
+      <Project /> {/* Fixed from <Projects /> to <Project /> */}
       <ClientsSlider />
       <Testimonials />
     </>
@@ -32,7 +32,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<Project />} /> {/* Fixed component name */}
         <Route path="/contact" element={<Contact />} />
       </Routes>
       
